b9a22f0af812c63059a524e6a99cb5c038765188
Ashish <ashish@Ashishs-MacBook-Pro.local>
2025-08-03 21:00:39 +0530

    calender updated with bg colour

--- src/pages/navbarPages/calender.jsx
+++ src/pages/navbarPages/calender.jsx
@@ -2,8 +2,8 @@ import CalendarGrid from "../navbarPages/calenderGrid";
 
 export default function Calendar() {
   return (
-    <div className="min-h-screen p-4 bg-white">
-      <h1 className="text-3xl font-bold text-center mb-4">📅 PathVibe Calendar</h1>
+    <div className="min-h-screen p-4 bg-gradient-to-br from-blue-50 to-indigo-700  ">
+      <h1 className="text-3xl font-bold text-center mb-4"> PathVibe Calendar</h1>
       <CalendarGrid />
     </div>
   );
